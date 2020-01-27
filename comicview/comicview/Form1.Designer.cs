59149763f94382b7a5285df7103609cfac666a0c
승연 이 <tfgui2@gmail.com>
2020-01-27 13:22:15 +0900

    ver 1.0

---
+++ comicview/comicview/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace comicview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(999, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.optionToolStripMenuItem.Text = "option";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(308, 265);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(496, 265);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 634);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ comicview/comicview/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace comicview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Load("il2.jpg");
+            //pictureBox2.Load("il2.jpg");
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string currentFile = openFileDialog1.FileName;
+                makeFileList(currentFile);
+                showTwo(m_currentIndex);
+            }
+        }
+
+        private string[] m_files;
+        private int m_currentIndex = 0;
+        private void makeFileList(string currentfile)
+        {
+            string folder = Path.GetDirectoryName(currentfile);
+            string[] files = Directory.GetFiles(folder);
+            //foreach (string name in files)
+            //{
+            //    string ext = Path.GetExtension(name).ToLower();
+            //    if ()
+            //}
+            m_files = files;
+            for (int i = 0; i < m_files.Length; i++)
+            {
+                if (currentfile == m_files[i])
+                {
+                    m_currentIndex = i;
+                    break;
+                }
+            }
+
+            this.Text = folder;
+        }
+
+        private int showTwo(int index)
+        {
+            if (index < 0)
+                return index;
+
+            if (index >= m_files.Length)
+                return index;
+
+            pictureBox2.Load(m_files[index]);
+            index++;
+
+            if (index >= m_files.Length)
+                return index;
+
+            pictureBox1.Load(m_files[index]);
+            index++;
+
+            return index;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            Image im1 = pictureBox1.Image;
+            if (im1 != null)
+            {
+                pictureBox1.Height = this.ClientSize.Height;
+                pictureBox1.Width = (int)(pictureBox1.Height * ((float)(im1.Width) / (float)(im1.Height)));
+                int x = this.ClientSize.Width / 2 - pictureBox1.Width;
+                pictureBox1.Location = new Point(x, 0);
+            }
+
+            Image im2 = pictureBox2.Image;
+            if (im2 != null)
+            {
+                pictureBox2.Height = this.ClientSize.Height;
+                pictureBox2.Width = (int)(pictureBox2.Height * ((float)(im2.Width) / (float)(im2.Height)));
+                int x = this.ClientSize.Width / 2;
+                pictureBox2.Location = new Point(x, 0);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                if (e.Modifiers == Keys.Control) 
+                {
+                    goNext(1);
+                }
+                else
+                {
+                    goNext(2);
+                }
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                if (e.Modifiers == Keys.Control)
+                {
+                    goPrev(1);
+                }
+                else
+                {
+                    goPrev(2);
+                }
+            }
+
+            Form1_ClientSizeChanged(this, new EventArgs());
+        }
+
+        private void goNext(int step)
+        {
+            this.m_currentIndex += step;
+            showTwo(m_currentIndex);
+        }
+
+        private void goPrev(int step)
+        {
+            this.m_currentIndex -= step;
+            showTwo(m_currentIndex);
+        }
+    }
+}
